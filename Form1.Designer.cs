97d82eeae9288317f978c7f0452dcad4ebd64a4c
Juan1329 <silva.juan.almeida@gmail.com>
2021-01-06 17:41:16 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtTelefone = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnApagar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.dgvResultado = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResultado)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Telefone";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(42, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Email";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(151, 40);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(125, 27);
+            this.txtNome.TabIndex = 3;
+            this.txtNome.TextChanged += new System.EventHandler(this.txtNome_TextChanged);
+            // 
+            // txtTelefone
+            // 
+            this.txtTelefone.Location = new System.Drawing.Point(151, 95);
+            this.txtTelefone.Name = "txtTelefone";
+            this.txtTelefone.Size = new System.Drawing.Size(125, 27);
+            this.txtTelefone.TabIndex = 4;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(151, 153);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(125, 27);
+            this.txtEmail.TabIndex = 5;
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Location = new System.Drawing.Point(42, 225);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(94, 29);
+            this.btnEnviar.TabIndex = 6;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Location = new System.Drawing.Point(42, 288);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(94, 29);
+            this.btnEditar.TabIndex = 7;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // btnApagar
+            // 
+            this.btnApagar.Location = new System.Drawing.Point(42, 352);
+            this.btnApagar.Name = "btnApagar";
+            this.btnApagar.Size = new System.Drawing.Size(94, 29);
+            this.btnApagar.TabIndex = 8;
+            this.btnApagar.Text = "Apagar";
+            this.btnApagar.UseVisualStyleBackColor = true;
+            this.btnApagar.Click += new System.EventHandler(this.btnApagar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(181, 351);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(94, 29);
+            this.btnSair.TabIndex = 9;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // dgvResultado
+            // 
+            this.dgvResultado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvResultado.Location = new System.Drawing.Point(317, 95);
+            this.dgvResultado.Name = "dgvResultado";
+            this.dgvResultado.RowHeadersWidth = 51;
+            this.dgvResultado.RowTemplate.Height = 29;
+            this.dgvResultado.Size = new System.Drawing.Size(453, 188);
+            this.dgvResultado.TabIndex = 10;
+            this.dgvResultado.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvResultado_CellContentClick);
+            this.dgvResultado.Click += new System.EventHandler(this.dgvResultado_Click);
+            this.dgvResultado.DoubleClick += new System.EventHandler(this.dgvResultado_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(317, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(419, 27);
+            this.textBox1.TabIndex = 11;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 406);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dgvResultado);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnApagar);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtTelefone);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResultado)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtTelefone;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnApagar;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.DataGridView dgvResultado;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conexao = new SqlConnection(@"Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=meubanco;Data Source=.\SQLEXPRESS");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtNome_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CarregarDados();
+        }
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            if (!Valida_campo())
+                return;
+            string sql = "insert into agendacs (nome, contato, email) values (@nome, @contato, @email)";
+            SqlCommand comando = new SqlCommand(sql, conexao);
+            conexao.Open();
+            comando.Parameters.AddWithValue("@nome", txtNome.Text);
+            comando.Parameters.AddWithValue("@contato", txtTelefone.Text);
+            comando.Parameters.AddWithValue("@email", txtEmail.Text);
+            comando.ExecuteNonQuery();
+            MessageBox.Show("Criado", "Salvar", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            conexao.Close();
+            CarregarDados();
+            Limpar();
+
+
+        }
+        private void CarregarDados()
+        {
+            conexao.Open();
+            DataTable dt = new DataTable();
+            SqlDataAdapter dados = new SqlDataAdapter("select * from agendacs", conexao);
+            dados.Fill(dt);
+            dgvResultado.DataSource = dt;
+            conexao.Close();
+
+        }
+
+        private void dgvResultado_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dgvResultado_Click(object sender, EventArgs e)
+        {
+            txtNome.Text = dgvResultado.CurrentRow.Cells[0].Value.ToString();
+            txtTelefone.Text = dgvResultado.CurrentRow.Cells[1].Value.ToString();
+            txtEmail.Text = dgvResultado.CurrentRow.Cells[2].Value.ToString();
+        }
+
+        private void dgvResultados_DoubleClick(object sender, EventArgs e)
+        {
+
+
+
+
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            if (Valida_campo())
+            {
+
+                string sql = "update agendacs set contato = @contato, email = @email where nome = @nome";
+                SqlCommand comando = new SqlCommand(sql, conexao);
+                conexao.Open();
+                comando.Parameters.AddWithValue("@nome", txtNome.Text);
+                comando.Parameters.AddWithValue("@contato", txtTelefone.Text);
+                comando.Parameters.AddWithValue("@email", txtEmail.Text);
+                comando.ExecuteNonQuery();
+                MessageBox.Show("Deseja editar esse contato", "Editar", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+                conexao.Close();
+                CarregarDados();
+                Limpar();
+            }
+        }
+        private void Limpar()
+        {
+
+            txtNome.Clear();
+            txtTelefone.Clear();
+            txtEmail.Clear();
+
+        }
+
+        private void btnApagar_Click(object sender, EventArgs e)
+        {
+
+
+            if (MessageBox.Show("Deseja apagar?", "Apagar", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+
+                SqlCommand delete = new SqlCommand("delete from agendacs where nome = @nome", conexao);
+                conexao.Open();
+                delete.Parameters.AddWithValue("@nome", dgvResultado.CurrentRow.Cells[0].FormattedValue);
+                delete.ExecuteNonQuery();
+                conexao.Close();
+                CarregarDados();
+
+
+            }
+            Limpar();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            conexao.Open();
+            DataTable dt = new DataTable();
+            SqlDataAdapter dados = new SqlDataAdapter($"select * from agendacs where nome like '{textBox1.Text}%'", conexao);
+            dados.Fill(dt);
+            dgvResultado.DataSource = dt;
+            conexao.Close();
+
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            if (!(MessageBox.Show("Deseja sair mesmo", "sair", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No))
+                this.Close();
+        }
+        private bool Valida_campo()
+        {
+            if (string.IsNullOrEmpty(txtNome.Text) && string.IsNullOrEmpty(txtTelefone.Text) && string.IsNullOrEmpty(txtEmail.Text))
+            {
+                MessageBox.Show("Todos campos são obrigatórios", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+
+            }
+            return true;
+        }
+    }
+}
+
